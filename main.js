9ce9e5020da7ed75004d2c20bd3ab0c68170ef29
Arpan Ghosh <arpan@itobuz.com>
2024-12-26 18:26:09 +0530

    initial-commit

---
+++ main.js
@@ -0,0 +1,23 @@
+function color1 (){
+
+    document.body.style.backgroundColor = "aqua";
+    
+}
+
+function color2 () {
+
+    document.body.style.backgroundColor = "red";
+
+}
+
+function color3 () {
+
+    document.body.style.backgroundColor = "yellow";
+
+}
+
+function color4 () {
+
+    document.body.style.backgroundColor = "green";
+
+}
